b671952659eb47c5453a00d5c1b7ebdcaaf9c5dc
Enes Gülcü <44102048+enesgulcu@users.noreply.github.com>
2023-07-08 17:15:59 +0300

    test

--- pages/api/test/index.js
+++ pages/api/test/index.js
@@ -1,25 +1,27 @@
 import { createNewData } from "@/services/serviceOperations";
 
 const handler = async (req, res) => {
-  if (req.method === "POST") {
-  
+  try {
+    if (req.method !== "POST") {
+      throw new Error("hata 1");
+    }
+
     const { email, password } = req.body;
 
-    if (email && password) {
-      
-      const user = await createNewData("user", { email: email, password: password });
-      if (user) {
-        res.status(200).json({ user });
-      } else {
-        res.status(400).json({ message: "Kullanıcı oluşturulamadı!" });
-      } 
-    } else{
-      res.status(400).json({ message: "Eksik bilgi!" });
+    if (!email || !password) {
+      throw new Error("hata 2");
     }
-  } else {
-    res.status(400).json({ message: "Yanlış istek!" });
-  }
 
+    const user = await createNewData("user", { email: email, password: password });
+    user && console.log("veri iletildi");
+    if (user.error || !user) {
+      throw new Error(user);
+    }
+    return res.status(200).json({status: "success",USER: user , message: "Şifre başarıyla değiştirildi!"});
+
+  } catch (error) {
+    return res.status(500).json({status: "error", error: error.message});
+  }
 };
 
 export default handler;
