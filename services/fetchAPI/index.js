cd9665dc984b45213e28efa8a037702aca8d9360
Enes Gülcü <enes.gulcu@hotmail.com>
2023-07-08 20:43:03 +0300

    fetch işlemi başarıyla gerçekleşti

--- services/fetchAPI/index.js
+++ services/fetchAPI/index.js
@@ -6,7 +6,7 @@ const postAPI = async (URL, body, method="POST", headers = {'Content-Type': 'app
         if(!process.env.NEXT_PUBLIC_API_URL || !URL){
             throw new Error("URL bulunamadı!");
         }
-        const data = await fetch (`${"api" + URL}`,{
+        const data = await fetch (`${process.env.NEXT_PUBLIC_API_URL + URL}`,{
             method: method,
             headers: headers,
             body: JSON.stringify(body),
