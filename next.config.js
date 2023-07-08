0493ab521f7afbf38aebe251ea21c83bb0131c04
Enes Gülcü <44102048+enesgulcu@users.noreply.github.com>
2023-07-08 16:06:22 +0300

    test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    experimental: {
+      appDir: true,
+    },
+    images: {
+      domains: ['source.unsplash.com'],
+    },
+  
+    i18n: {
+      locales: ['tr-TR'],
+      
+      defaultLocale: 'tr-TR',
+      domains: [
+        {
+          domain: process.env.NEXT_PUBLIC_URL,
+          defaultLocale: 'tr-TR',
+  
+          https: true,
+        }
+      ],
+    },
+    
+  }
+  
+  module.exports = nextConfig
+  
