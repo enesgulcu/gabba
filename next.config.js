70120caa8a6b4b764ec2992012286268a3bae861
Enes Gülcü <44102048+enesgulcu@users.noreply.github.com>
2023-07-08 16:10:58 +0300

    test

--- next.config.js
+++ next.config.js
@@ -7,19 +7,6 @@ const nextConfig = {
       domains: ['source.unsplash.com'],
     },
   
-    i18n: {
-      locales: ['tr-TR'],
-      
-      defaultLocale: 'tr-TR',
-      domains: [
-        {
-          domain: process.env.NEXT_PUBLIC_URL,
-          defaultLocale: 'tr-TR',
-  
-          https: true,
-        }
-      ],
-    },
     
   }
   
