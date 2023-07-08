9297a1f6391eca1509cd9bce16b41eb782b462c8
Enes Gülcü <44102048+enesgulcu@users.noreply.github.com>
2023-07-08 16:57:56 +0300

    test

--- next.config.js
+++ next.config.js
@@ -2,11 +2,19 @@
 const nextConfig = {
     experimental: {
       appDir: true,
+      // matching all API routes
+      source: "/api/:path*",
+      headers: [
+        { key: "Access-Control-Allow-Credentials", value: "true" },
+        { key: "Access-Control-Allow-Origin", value: "*" },
+        { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+        { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+      ]
     },
     images: {
       domains: ['source.unsplash.com'],
     },
-  
+    
     
   }
   
