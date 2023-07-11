448e5d31e9e2a799ca1f10e4565b519f11b772fd
Enes Gülcü <44102048+enesgulcu@users.noreply.github.com>
2023-07-11 16:23:18 +0300

    backend'de veri kontrlü yapıldı.
    yup entegre edilecek...

--- components/test_form/index.js
+++ components/test_form/index.js
@@ -4,60 +4,64 @@ import {postAPI, getAPI} from '@/services/fetchAPI';
 import { Formik, Form, FieldArray, Field } from "formik";
 import { useState , useEffect} from 'react';
 import { MdOutlineCancel } from "react-icons/md";
+import Table from '@/components/table';
 
  const Test_form = () => {
 
+  // veri tabanından çekilen verilerin tutulacağı state.
   const [listData, setListData] = useState();  
+  
+  // veri tabanından verileri çekme işlemini yapan fonksiyon.
+  const fetchData = async () => {
+    const responseData = await getAPI("/test");
+    if (!responseData || responseData.status !== "success" || responseData.status == "error") {
+      return "VERİ YOK!";
+    } else {
+      setListData(responseData.data);
+    }
+  }
 
   useEffect(() => {
-    const fetchData = async () => {
-      const responseData = await getAPI("/test");
-      if (!responseData || responseData.status !== "success" || responseData.status == "error") {
-        return "VERİ YOK!";
-      } else {
-        setListData(responseData.data);
-      }
-    }
 
+    // didMount anında -> veri tabanından verileri çekme işlemini başlat.
     fetchData();
 
-
   }, [])
-
     
-    
+    // formda kullanılacak ölçümlerin tutulacağı state.
     const [measurements, setMeasurements] = useState([
-        { firstValue: "",secondValue: "", unit: 'cm', isRangeEnabled: true },
+        { 
+          firstValue: "",
+          secondValue: "", 
+          unit: 'cm', 
+          oneRangeEnabled: false,
+          twoRangeEnabled: true,
+          manuelDefined: false,
+          turkish: '',
+          ukrainian: '',
+          english: '',
+         },
     ]);
-
     
+
+    // ölçü ekleme fonksiyonu.
     const addMeasurement = () => {
-        setMeasurements([...measurements, { firstValue: "", secondValue: "", unit: 'cm', isRangeEnabled: true }]);
+        setMeasurements([
+          ...measurements, 
+          { 
+            firstValue: "",
+            secondValue: "", 
+            unit: 'cm', 
+            oneRangeEnabled: false,
+            twoRangeEnabled: true,
+            manuelDefined: false,
+            turkish: '',
+            ukrainian: '',
+            english: '',
+          }
+        ]);
       };
 
-    const removeMeasurement = () => {
-        const newMeasurements = [...measurements];
-        newMeasurements.forEach((measurement, index) => {
-          if(newMeasurements[index] && newMeasurements[index].firstValue === ""){
-            //eğer ilk değer boş ise bu ölçümü sil
-            
-            
-            const filteredData = newMeasurements.filter(item => item.firstValue !== "");
-            if(filteredData.length === 0){
-              filteredData.push({ firstValue: "", secondValue: "", unit: 'cm', isRangeEnabled: true })
-            }
-            setMeasurements(filteredData);
-            
-          }
-          else if (newMeasurements[index] && !newMeasurements[index].isRangeEnabled){
-            newMeasurements[index].secondValue = ""
-            setMeasurements(newMeasurements);
-          }
-          
-        });
-        
-        
-      };
 
   return (
     <div className='w-full'>
@@ -69,13 +73,15 @@ import { MdOutlineCancel } from "react-icons/md";
 
         onSubmit={async () => {
           const data = measurements;
-
-          const responseData = await postAPI("/test", data);
-          if (responseData.status !== "success" || responseData.status == "error") {
-            console.log(responseData.error);
-          } else {
-            console.log("işlem başarılı!");
-          }
+          console.log(data);
+            const responseData = await postAPI("/test", data);
+            if (responseData.status !== "success" || responseData.status == "error") {
+              console.log(responseData.error);
+            } else {
+              console.log("işlem başarılı!");
+            }
+          
+          
         }}
       >
         {(props) => (
@@ -85,26 +91,34 @@ import { MdOutlineCancel } from "react-icons/md";
                     {measurements.map((measurement, index) => (
                       <div
                         key={index}
-                        className={`hover:bg-yellow-400 py-4 transition-all w-full flex-col md:flex-row flex flex-wrap justify-center items-center gap-4 ${
-                          index % 2 ? "bg-gray-100" : "bg-white"
+                        className={` lg:px-10 px-2 hover:bg-yellow-400 py-4 transition-all w-full flex-col xl:flex-row flex flex-wrap xl:justify-between justify-center gap-4 ${
+                          index % 2 ? "bg-white" : "bg-gray-100"
                         }`}
                       >
+                        
                         <label
                           htmlFor={`measure-${index}`}
-                          className="inline-block whitespace-nowrap font-semibold"
+                          className="whitespace-nowrap font-semibold flex justify-center items-center"
                         >
-                          {measurements[index].isRangeEnabled
-                            ? <div className='flex justify-start items-center flex-row gap-2'><span className='flex justify-center items-center w-[25px] h-[25px] rounded-full bg-black text-white'>{`${index + 1}`}</span> - Ölçü Aralığı Ekle</div>
-                            : <div className='flex justify-start items-center flex-row gap-2'><span className='flex justify-center items-center w-[25px] h-[25px] rounded-full bg-black text-white'>{`${index + 1}`}</span> - Ölçü Ekle</div>}
-                        </label>
+                          {
+                          measurements[index].oneRangeEnabled ? 
+                          <div className='flex justify-start items-center flex-row gap-2'><span className='flex justify-center items-center w-[25px] h-[25px] rounded-full bg-black text-white'>{`${index + 1}`}</span> - Tek Ölçü Ekle</div>
+                          : measurements[index].twoRangeEnabled ?
+                          <div className='flex justify-start items-center flex-row gap-2'><span className='flex justify-center items-center w-[25px] h-[25px] rounded-full bg-black text-white'>{`${index + 1}`}</span> - Ölçü Aralığı Ekle</div>
+                          :
+                          <div className='flex justify-start items-center flex-row gap-2'><span className='flex justify-center items-center w-[25px] h-[25px] rounded-full bg-black text-white'>{`${index + 1}`}</span> - Özel Ölçü Ekle</div>
+                          }
+                          </label>
 
-                        <div className="flex items-center gap-2 lg:gap-10 xl:gap-16 flex-wrap flex-col md:flex-row">
+                        <div className=' flex flex-row flex-wrap lg:flex-nowrap gap-4 justify-center items-center'>
                           <input
                             id={`measurements.${index}.firstValue`}
                             name={`measurements.${index}.firstValue`}
-                            type="number"
+                            type={`${measurements[index].manuelDefined ? "text" : "number"}`}
                             className={`border border-gray-300 rounded-md p-2 `}
-                            placeholder="Ölçü Değeri"
+                            placeholder={`${
+                              measurements[index].manuelDefined ? "örnek: Soldan Kapak Çıkar" : "örnek: 124"   
+                            } `}
                             value={measurements[index].firstValue || ""}
                             
                             onChange={(e) => {
@@ -120,11 +134,11 @@ import { MdOutlineCancel } from "react-icons/md";
                             name={`measurements.${index}.secondValue`}
                             type="number"
                             className={`border border-gray-300 rounded-md p-2 ${
-                              measurements[index].isRangeEnabled
+                              measurements[index].twoRangeEnabled
                                 ? "block"
                                 : "hidden"
                             }`}
-                            placeholder="Ölçü Değeri"
+                            placeholder="örnek: 238"
                             value={measurements[index].secondValue || ""}
                             onChange={(e) => {
                               const newMeasurements = [...measurements];
@@ -133,47 +147,111 @@ import { MdOutlineCancel } from "react-icons/md";
                               setMeasurements(newMeasurements);
                             }}
                           />
+                        </div>
+                          <div className="flex flex-row flex-wrap justify-center xl:justify-around gap-4 items-center cursor-pointer">
+                            
+                            <select
+                                id={`measurements.${index}.unit`}
+                                name={`measurements.${index}.unit`}
+                                disabled={measurements[index].manuelDefined}
+                                defaultValue="cm"
+                                className={`${measurements[index].manuelDefined ? " opacity-30" : "block"} cursor-pointer  p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+                                onChange={(e) => {
+                                  const newMeasurements = [...measurements];
+                                  newMeasurements[index].unit = e.target.value;
+                                  setMeasurements(newMeasurements);
+                                }}
+                              >
+                                <option value="cm">cm</option>
+                                <option value="mm">mm</option>
+                                <option value="m">m</option>
+                            </select>
+                            <div 
+                              onClick={() => {
+                                if(!measurement[index]?.oneRangeEnabled){
+                                  const newMeasurements = [...measurements];
+                                  newMeasurements[index].oneRangeEnabled = true;
+                                  newMeasurements[index].twoRangeEnabled = false;
+                                  newMeasurements[index].manuelDefined = false;
 
-                          <div className="flex flex-row flex-wrap gap-4 justify-center items-center">
-                            <div className="flex gap-4">
+                                  setMeasurements(newMeasurements);
+                                }
+                              }}
+                              className={`${measurements[index].oneRangeEnabled ? "bg-blue-200 text-black" : "bg-white cursor-pointer"} cursor-pointer flex justify-center items-center gap-2 border border-gray-300 p-2 rounded-lg`}
+                            > 
                               <label
-                                className="inline-block whitespace-nowrap"
+                                  className="inline-block whitespace-nowrap cursor-pointer"
+                                  htmlFor={`measure-${index}`}
+                                >
+                                  Tek Ölçü:
+                              </label>
+
+                                <input
+                                  type="checkbox"
+                                  id={`measurements.${index}.oneRangeEnabled`}
+                                  name={`measurements.${index}.oneRangeEnabled`}
+                                  className="border border-gray-300 rounded-md p-2 w-6 h-6 cursor-pointer"
+                                  checked={measurement.oneRangeEnabled}
+                                  readOnly={true}
+                                />
+                              </div>
+                              <div 
+                                onClick={() => {
+                                  if(!measurement[index]?.twoRangeEnabled){
+                                    const newMeasurements = [...measurements];
+                                    newMeasurements[index].twoRangeEnabled = true;
+                                    newMeasurements[index].oneRangeEnabled = false;
+                                    newMeasurements[index].manuelDefined = false;
+                                    setMeasurements(newMeasurements);
+                                  }
+                                }}
+
+                                className={`${measurements[index].twoRangeEnabled ? "bg-blue-200 text-black" : "bg-white cursor-pointer"} cursor-pointer flex justify-center items-center gap-2 border border-gray-300 p-2 rounded-lg`}
+                              >  
+                              <label
+                                className="inline-block whitespace-nowrap cursor-pointer"
                                 htmlFor={`measure-${index}`}
                               >
-                                Ölçü Aralığı Gir:{" "}
+                                Ölçü Aralığı:
                               </label>
 
                               <input
                                 type="checkbox"
-                                id={`measurements.${index}.isRangeEnabled`}
-                                name={`measurements.${index}.isRangeEnabled`}
-                                className="border border-gray-300 rounded-md p-2 w-6 h-6"
-                                checked={measurement.isRangeEnabled}
-                                onChange={(e) => {
-                                  const newMeasurements = [...measurements];
-                                  newMeasurements[index].isRangeEnabled =
-                                    e.target.checked;
-                                  setMeasurements(newMeasurements);
-                                }}
+                                id={`measurements.${index}.twoRangeEnabled`}
+                                name={`measurements.${index}.twoRangeEnabled`}
+                                className="border border-gray-300 rounded-md p-2 w-6 h-6 cursor-pointer"
+                                checked={measurement.twoRangeEnabled}
+                                readOnly={true}
                               />
-                            </div>
+                              </div>
+                              <div 
+                                onClick={() => {
+                                  if(!measurement[index]?.manuelDefined){
+                                    const newMeasurements = [...measurements];
+                                    newMeasurements[index].manuelDefined = true;
+                                    newMeasurements[index].oneRangeEnabled = false;
+                                    newMeasurements[index].twoRangeEnabled = false;
+                                    setMeasurements(newMeasurements);
+                                  }
+                                }}
+                                className={`${measurements[index].manuelDefined ? "bg-blue-200 text-black" : "bg-white"} cursor-pointer flex justify-center items-center gap-2 border border-gray-300 p-2 rounded-lg`}
+                              > 
+                              <label
+                                className="inline-block whitespace-nowrap cursor-pointer"
+                                htmlFor={`measure-${index}`}
+                              >
+                                Özel Ölçü
+                              </label>
 
-                            <select
-                              id={`measurements.${index}.unit`}
-                              name={`measurements.${index}.unit`}
-                              defaultValue="cm"
-                              className="block p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                              onChange={(e) => {
-                                const newMeasurements = [...measurements];
-                                newMeasurements[index].unit = e.target.value;
-                                setMeasurements(newMeasurements);
-                              }}
-                            >
-                              <option value="cm">cm</option>
-                              <option value="mm">mm</option>
-                              <option value="m">m</option>
-                            </select>
-
+                              <input
+                                type="checkbox"
+                                id={`measurements.${index}.manuelDefined`}
+                                name={`measurements.${index}.manuelDefined`}
+                                className="border border-gray-300 rounded-md p-2 w-6 h-6 cursor-pointer"
+                                checked={measurement.manuelDefined}
+                                readOnly={true}
+                              />
+                              </div>
                             <button
                               type="button"
                               onClick={() => {
@@ -188,25 +266,24 @@ import { MdOutlineCancel } from "react-icons/md";
                                 <MdOutlineCancel size={25} />{" "}
                               </p>
                             </button>
-                          </div>
+                          
                         </div>
                       </div>
                     ))}
                   </div>
 
-                  <div className="w-full flex justify-center items-center gap-4 my-6">
+                  <div className="w-full flex justify-center items-center gap-10 my-6 scale-125">
                     <button
                       type="button"
                       onClick={addMeasurement}
-                      className="px-3 py-2 rounded-md bg-blue-500 text-white"
+                      className="px-3 py-2 rounded-md bg-blue-500 text-white hover:rotate-2 hover:scale-105 transition-all"
                     >
                       Yeni Ölçü Ekle
                     </button>
 
                     <button
-                      onClick={() => removeMeasurement()}
                       type="submit"
-                      className="px-4 py-2 rounded-md bg-green-500 text-white"
+                      className="px-4 py-2 rounded-md bg-green-500 text-white hover:rotate-2 hover:scale-105 transition-all"
                     >
                       Gönder
                     </button>
@@ -218,14 +295,20 @@ import { MdOutlineCancel } from "react-icons/md";
         <div className='w-full flex justify-center items-center p-2 bg-black text-white font-bold text-xl'>
           <h4>Ölçü Listesi</h4>
         </div>
+
+        <Table/>                     
+        {/* <div>
         {
-          listData &&   listData.map((item, index) => (
+          listData && listData.map((item, index) => (
             <div key={index} className={`p-4 ${index % 2 ? "bg-white" : "bg-gray-100"}`}>
-              {index+1} - <span className='ml-4 bg-blue-100 p-2 rounded-lg'>{item.firstValue} - {item.secondValue} {item.unit}</span>
+              {index+1} - <span className='ml-4 bg-blue-100 p-2 rounded-lg'>{item.firstValue}  {item.secondValue && " - " + item.secondValue} {item.unit}</span>
             </div>
           
           ))
         }
+        </div> */}
+
+
       </div>
     </div>
   );
--- pages/api/test/index.js
+++ pages/api/test/index.js
@@ -1,5 +1,89 @@
 import { createNewData, getAllData, createNewDataMany } from "@/services/serviceOperations";
 
+// girilen verileri göndermeden önce kontrol ederiz.
+const checkData = async (data) => {
+
+  const newMeasurements = data.filter(item => item.firstValue !== "");
+
+  newMeasurements.forEach((measurement, index) => {
+    if(newMeasurements[index]){
+
+//####################################################################################
+      // ######### sadece " Tek Ölçü Ekleme Aktif " ise burası çalışır
+      if(measurement.oneRangeEnabled){
+
+        // eğer ölçü aralığı aktif değilse ve ikinci değer doluysa
+        // ikinci değeri boşalt.
+        if(measurement.secondValue){
+          newMeasurements[index].secondValue = "";
+        }
+
+        // eğer bir ölçü varsa ancak unit değeri boş ise
+        // unit değerini default olarak " cm " ata.
+        if(!measurement.unit){
+          newMeasurements[index].unit = 'cm';
+        }
+
+        // Tek Ölçü Ekleme bölümünde dil olamaz!
+        if(measurement.turkish || measurement.ukrainian || measurement.english){
+          newMeasurements[index].turkish = '';
+          newMeasurements[index].ukrainian = '';
+          newMeasurements[index].english = '';
+        }
+
+//####################################################################################
+        // ######### sadece "Ölçü Aralığı Ekleme Aktif" ise burası çalışır
+        else if(measurement.twoRangeEnabled){
+
+          // eğer ölçü aralığı ekleme aktifse ve ikinci değer boşsa
+          // ölçü aralığı ekleme aktifliğini kapat.
+          if(measurement.twoRangeEnabled && !measurement.secondValue){
+            newMeasurements[index].twoRangeEnabled = false;
+          }
+
+          // eğer bir ölçü varsa ancak unit değeri boş ise
+          // unit değerini default olarak " cm " ata.
+          if(!measurement.unit){
+            newMeasurements[index].unit = 'cm';
+          }
+
+          //Ölçü Aralığı Ekleme bölümünde dil olamaz!
+          if(measurement.turkish || measurement.ukrainian || measurement.english){
+            newMeasurements[index].turkish = '';
+            newMeasurements[index].ukrainian = '';
+            newMeasurements[index].english = '';
+          }
+
+        }
+
+//####################################################################################
+        // ######### sadece " Manuel Ölçü Tanımlama Aktif " ise burası çalışır
+        else if(measurement.manuelDefined){
+
+          if(measurement.secondValue){
+            newMeasurements[index].secondValue = false;
+          }
+
+          // eğer unit üzerinde bir ölçü varsa
+          // unit değerini boşalt.
+          if(measurement.unit){
+            newMeasurements[index].unit = '';
+          }
+        }
+      }
+    }
+    // yapılan kontroller sonrası güncel düzenlenmiş veriyi göndeririz.
+    
+  });
+  
+  if(newMeasurements.length > 0){
+    return newMeasurements;
+  }
+  else{
+    return false;
+  }
+};
+
 const handler = async (req, res) => {
   try {
     if (req.method === "POST") {
@@ -7,10 +91,15 @@ const handler = async (req, res) => {
       const data = req.body;
       if(!data){
         throw "Veri alınamadı";
-      }   
+      } 
+      
+      // gelen verinin doğruluğunu kontrol ediyoruz.
+      const checkedData = await checkData(data);
 
-      console.log(data);
-      const createdNewData = await createNewDataMany("measurements", data);
+      if(!checkedData){
+        return res.status(500).json({ status: "error", error: "veri yok"});
+      }
+      const createdNewData = await createNewDataMany("measurements", checkedData);
 
       if(!createdNewData || createdNewData.error){
         throw createdNewData;
